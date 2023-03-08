6554819186a5aba2d3aff4bb62dabeb11ecbf2a0
CarlosAndres <solrak41@gmail.com>
2023-03-08 16:24:00 -0500

    Refactor code. Add pop ups logic to popups.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './style.css';
 import { userFactory } from './user';
+import { toggleNamePopUp } from './popups';
 
 const newUser = userFactory('User', []);
 const welcomeUser = document.querySelector('.sidebar-user strong');
 welcomeUser.textContent = newUser.name;
 
 // Edit name popup
+
 let popup = document.querySelector('.edit-user');
-popup.addEventListener('click', function () {
-    let popupForm = document.querySelector('.edit-form');
-    popupForm.classList.toggle('show');
-});
+popup.addEventListener('click', toggleNamePopUp);
 
 console.log('Testing');
---
+++ src/popups.js
@@ -0,0 +1,6 @@
+function toggleNamePopUp() {
+    let popupForm = document.querySelector('.edit-form');
+    popupForm.classList.toggle('show');
+}
+
+export { toggleNamePopUp };
