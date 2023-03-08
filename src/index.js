eb326cddb5bcdd5b2b72b0a7e6b48b0c5f689a40
CarlosAndres <solrak41@gmail.com>
2023-03-08 16:48:29 -0500

    Add functionality to allow user to edit username

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import { userFactory } from './user';
 import { toggleNamePopUp } from './popups';
 
-const newUser = userFactory('User', []);
+const newUser = userFactory('User', {});
 const welcomeUser = document.querySelector('.sidebar-user strong');
 welcomeUser.textContent = newUser.name;
 
@@ -12,3 +12,11 @@ editNamePopup.addEventListener('click', toggleNamePopUp);
 // Edit name cancel button
 let nameCancel = document.querySelector('.form-cancel');
 nameCancel.addEventListener('click', toggleNamePopUp);
+// Edit name
+let nameConfirm = document.querySelector('.form-check');
+nameConfirm.addEventListener('click', () => {
+    let nameInput = document.getElementById('name');
+    newUser.name = nameInput.value;
+    welcomeUser.textContent = newUser.name;
+});
+nameConfirm.addEventListener('click', toggleNamePopUp);
