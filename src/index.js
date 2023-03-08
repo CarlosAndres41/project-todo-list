c10483dd293e190898ce13fdb708d9b3d72c867c
CarlosAndres <solrak41@gmail.com>
2023-03-08 16:12:09 -0500

    Add a popup to enter custom user name

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ const newUser = userFactory('User', []);
 const welcomeUser = document.querySelector('.sidebar-user strong');
 welcomeUser.textContent = newUser.name;
 
+// Edit name popup
+let popup = document.querySelector('.edit-popup');
+popup.addEventListener('click', function () {
+    let popupForm = document.querySelector('.edit-form');
+    popupForm.classList.toggle('show');
+});
+
 console.log('Testing');
