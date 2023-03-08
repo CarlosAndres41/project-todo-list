8fc43edc32d23472388cc666a5b3eaee3ec176d9
CarlosAndres <solrak41@gmail.com>
2023-03-08 18:00:40 -0500

    Create projects from user.projects object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style.css';
 import { userFactory } from './user';
 import { toggleNamePopUp } from './popups';
+import { addProject } from './my-projects';
 
 const newUser = userFactory('User', {});
 const welcomeUser = document.querySelector('.sidebar-user strong');
 welcomeUser.textContent = newUser.name;
+
+//Add default projects to user
+newUser.projects['Home'] = {};
+newUser.projects['Work'] = {};
+
+console.log(newUser.projects);
 
 // Edit name popup
 let editNamePopup = document.querySelector('.edit-user');
@@ -20,3 +27,8 @@ nameConfirm.addEventListener('click', () => {
     welcomeUser.textContent = newUser.name;
 });
 nameConfirm.addEventListener('click', toggleNamePopUp);
+
+// Create Projects
+Object.keys(newUser.projects).forEach((project) => {
+    addProject(project);
+});
