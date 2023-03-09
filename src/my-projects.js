f2d22b48c8a168b0e183beb7e2040a5a6b6c6065
CarlosAndres <solrak41@gmail.com>
2023-03-08 19:17:22 -0500

    Style .project divs

--- src/my-projects.js
+++ src/my-projects.js
@@ -12,6 +12,7 @@ function addProject(projectName) {
     let logo1 = document.createElement('i');
     logo1.classList.add('fa-solid');
     logo1.classList.add('fa-pen');
+    logo1.classList.add('fa-2xs');
     span1.appendChild(logo1);
     let span2 = document.createElement('span');
     span2.classList.add('edit-project');
