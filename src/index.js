cdfd6382e3cbad565bcbc8b7bbd9a650060cd45c
CarlosAndres <solrak41@gmail.com>
2023-03-10 16:03:16 -0500

    Create .intro

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { renderProjects } from './my-projects';
 
 const newUser = userFactory('User');
 displayName(newUser);
-
 editUserName(newUser);
 addProject(newUser);
 renderProjects(newUser);
