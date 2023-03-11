aa49927c69e108389c0078294806129471fff33c
CarlosAndres <solrak41@gmail.com>
2023-03-10 22:30:14 -0500

    Remove main-display first child when a project is clicked

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './style.css';
 import { userFactory, displayName } from './user';
 import { editUserName, addProject } from './popups';
 import { renderProjects } from './my-projects';
+import { showTasks } from './tasks';
 
 const newUser = userFactory('User');
 displayName(newUser);
 editUserName(newUser);
 addProject(newUser);
 renderProjects(newUser);
+showTasks();
--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,20 @@
-function showTasks() {
+function clearMainDisplay() {
     const mainDisplay = document.querySelector('.main-display');
     mainDisplay.removeChild(mainDisplay.firstElementChild);
 }
+
+function createDiv() {
+    const tasksDiv = document.createElement('div');
+    tasksDiv.classList.add('tasks-div');
+}
+
+function showTasks() {
+    let projects = document.querySelectorAll('.project p');
+    projects.forEach((project) => {
+        project.addEventListener('click', () => {
+            clearMainDisplay();
+        });
+    });
+}
+
+export { showTasks };
