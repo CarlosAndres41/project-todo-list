a13af12f837de2ed130417f13b6e4e1139d42970
CarlosAndres <solrak41@gmail.com>
2023-03-10 23:23:30 -0500

    Complete logic to display each project's tasks on main display when clicked

--- src/tasks.js
+++ src/tasks.js
@@ -20,30 +20,58 @@ function createDiv(projectName, tasks) {
         const taskDescription = document.createElement('div');
         taskDescription.classList.add('.task-description');
 
+        const checkBox = document.createElement('input');
+        checkBox.setAttribute('type', 'checkbox');
+        const description = document.createElement('p');
+        description.textContent = task.description;
+        taskDescription.appendChild(checkBox);
+        taskDescription.appendChild(description);
+
         const taskDate = document.createElement('div');
         taskDate.classList.add('.task-date');
+
+        const date = document.createElement('p');
+        date.textContent = task.date;
+        taskDate.appendChild(date);
 
         const taskIcons = document.createElement('div');
         taskIcons.classList.add('.task-icons');
 
+        let span1 = document.createElement('span');
+        span1.classList.add('high-priority');
+        let logo1 = document.createElement('i');
+        logo1.classList.add('fa-solid');
+        logo1.classList.add('fa-exclamation');
+        span1.appendChild(logo1);
+        if (task.priority === 'high') {
+            taskIcons.appendChild(span1);
+        }
+        let span2 = document.createElement('span');
+        span2.classList.add('delete-task');
+        let logo2 = document.createElement('i');
+        logo2.classList.add('fa-regular');
+        logo2.classList.add('fa-trash-can');
+        span2.appendChild(logo2);
+        taskIcons.appendChild(span2);
+
         individualTask.appendChild(taskDescription);
         individualTask.appendChild(taskDate);
         individualTask.appendChild(taskIcons);
+
+        tasksDiv.appendChild(individualTask);
     });
 
-    tasksDiv.appendChild(project);
+    display.appendChild(project);
     display.appendChild(tasksDiv);
 }
 
 function showTasks(user) {
     let projects = document.querySelectorAll('.project p');
     projects.forEach((project, index) => {
-        console.log(index);
         project.addEventListener('click', () => {
             clearMainDisplay();
             let projectName = project.classList[0].split('-')[0];
             let tasks = user.projects[index].tasks;
-            console.log(tasks);
             createDiv(projectName, tasks);
         });
     });
